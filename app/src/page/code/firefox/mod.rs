f051222f1d741dd40bc7783856ebaedcb81b83c9
mayiwen <i@mayiwen.com>
2025-12-26 02:37:56 -1200

    2025-12-26

--- app/src/page/code/firefox/mod.rs
+++ app/src/page/code/firefox/mod.rs
@@ -16,17 +16,19 @@ pub fn I() -> impl IntoView {
             <h1>
                 火狐插件 mayiwen
             </h1>
-             <p>"可以火狐插件中搜索“mayiwen”找到此插件"</p>
+
             <myw::Gap h=30/>
             <myw::button::I style="transform: scale(1.4); transform-origin: center"  on:click=move |_| {
                 open_url("https://addons.mozilla.org/zh-CN/firefox/addon/mayiwen/");
             }>获取开源软件</myw::button::I>
             <myw::Gap h=30/>
+              <p>"可以火狐插件中搜索“mayiwen”找到此插件"</p>
             <p>"实现的功能："</p>
             <p>"1. 点击插件的图标，可以关闭其他标签页、关闭左侧标签页、关闭右侧标签页。"</p>
             <p>"2. 在标签处右键，出现关闭其他标签选项。"</p>
             <p>"3. 在网页右键，出现关闭其他标签选项。"</p>
             <p>"4. 使用快捷键 CTRL + Q，关闭其他标签。"</p>
+             <myw::Gap />
             <div>
                 <img style="width: 100%; max-width: 1000px;" src="img/firefox/firefox.png" alt="主界面" />
             </div>
